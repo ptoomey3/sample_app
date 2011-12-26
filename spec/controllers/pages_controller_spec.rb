3bf05108d424f77054fbf5b004669cc73b8139f3
Patrick Toomey <ptoomey3@biasedcoin.com>
2011-12-26 10:33:47 -0600

    Dont with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,29 @@
-require 'spec_helper'
+  require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
-      response.should be_success
+      response.should have_selector("title",
+                                    :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
-      response.should be_success
+      response.should have_selector("title",
+                                    :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
