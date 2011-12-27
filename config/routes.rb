5f60447445adc0f634ca0892084542a1ff585e3d
Patrick Toomey <ptoomey3@biasedcoin.com>
2011-12-27 10:48:23 -0600

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  
   root :to => 'pages#home'
   # get "pages/home"
 
